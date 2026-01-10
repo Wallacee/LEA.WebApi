d50f5fe32edf5574e7a786fda7f90e78d168dacc
Wallace Veridiano de Jesus <wallacev2@gmail.com>
2026-01-10 12:38:40 -0300

    [DEVOPS] test

--- LEA.WebApi.Web/Controllers/AnalysisController.cs
+++ LEA.WebApi.Web/Controllers/AnalysisController.cs
@@ -29,7 +29,7 @@ namespace LEA.WebApi.Web.Controllers
         [Route("TesteApi")]
         public IActionResult Teste()
         {
-            return StatusCode(200, "A API está no ar, mano!!");
+            return StatusCode(200, "A API está no ar, maninho!!");
         }
 
         [HttpGet]
