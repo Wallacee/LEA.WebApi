e0738b4c1f7b055335d11b133c17fdbe87fe3811
Wallace Veridiano de Jesus <wallacev2@gmail.com>
2026-01-10 16:17:14 -0300

    [DEVOPS] test

--- LEA.WebApi.Web/Controllers/AnalysisController.cs
+++ LEA.WebApi.Web/Controllers/AnalysisController.cs
@@ -29,7 +29,7 @@ namespace LEA.WebApi.Web.Controllers
         [Route("TesteApi")]
         public IActionResult Teste()
         {
-            return StatusCode(200, "A API está no ar, viado da try!!");
+            return StatusCode(200, "A API está no ar, viado da try maker!!");
         }
 
         [HttpGet]
